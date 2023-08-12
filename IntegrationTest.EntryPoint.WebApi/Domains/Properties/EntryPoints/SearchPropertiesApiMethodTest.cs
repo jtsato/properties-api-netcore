bb8e1395991aa046537dd590ba8817fba767c92f
jtsato <jorge.takeshi.sato@gmail.com>
2023-08-12 01:24:44 -0300

    feat(properties-api): work in progress

--- IntegrationTest.EntryPoint.WebApi/Domains/Properties/EntryPoints/SearchPropertiesApiMethodTest.cs
+++ IntegrationTest.EntryPoint.WebApi/Domains/Properties/EntryPoints/SearchPropertiesApiMethodTest.cs
@@ -66,8 +66,8 @@ public class SearchPropertiesApiMethodTest
     }
 
     [Trait("Category", "Entrypoint (WebApi) Integration tests")]
-    [Fact(DisplayName = "GET /api/properties/search should return 200")]
-    public async Task SuccessfulToSearchProperties()
+    [Fact(DisplayName = "GET /api/properties/search should return 206 when there is partial content")]
+    public async Task SuccessfulToSearchPropertiesReturningPartialContent()
     {
         // Arrange
         SearchPropertiesQueryBuilder queryBuilder = new SearchPropertiesQueryBuilder();
@@ -84,6 +84,10 @@ public class SearchPropertiesApiMethodTest
             .WithToNumberOfToilets(2)
             .WithFromNumberOfGarages(1)
             .WithToNumberOfGarages(1)
+            .WithFromArea(100)
+            .WithToArea(200)
+            .WithFromBuiltArea(80)
+            .WithToBuiltArea(160)
             .WithFromSellingPrice(100000)
             .WithToSellingPrice(200000)
             .WithFromRentalTotalPrice(1000)
@@ -166,6 +170,10 @@ public class SearchPropertiesApiMethodTest
             NumberOfToiletsMax = 2,
             NumberOfGaragesMin = 1,
             NumberOfGaragesMax = 1,
+            AreaMin = 100,
+            AreaMax = 200,
+            BuiltAreaMin = 80,
+            BuiltAreaMax = 160,
             SellingPriceMin = 100000,
             SellingPriceMax = 200000,
             RentalTotalPriceMin = 1000,
@@ -233,5 +241,267 @@ public class SearchPropertiesApiMethodTest
             .AndExpectThat(JsonFrom.Path("$.pageable.numberOfElements"), Is<int>.EqualTo(1))
             .AndExpectThat(JsonFrom.Path("$.pageable.totalOfElements"), Is<int>.EqualTo(2))
             .AndExpectThat(JsonFrom.Path("$.pageable.totalPages"), Is<int>.EqualTo(2));
+    }
+
+    [Trait("Category", "Entrypoint (WebApi) Integration tests")]
+    [Fact(DisplayName = "GET /api/properties/search should return 204 when there is no content")]
+    public async Task SuccessfulToSearchPropertiesReturningNoContent()
+    {
+        // Arrange
+        SearchPropertiesQueryBuilder queryBuilder = new SearchPropertiesQueryBuilder();
+
+        queryBuilder
+            .WithType("Apartment")
+            .WithTransaction("Rent")
+            .WithRefId("REF 101")
+            .WithCity("White Duck")
+            .WithDistricts(new List<string> {"Downtown", "Alta Vista"})
+            .WithFromNumberOfBedrooms(0)
+            .WithToNumberOfBedrooms(3)
+            .WithFromNumberOfToilets(1)
+            .WithToNumberOfToilets(2)
+            .WithFromNumberOfGarages(1)
+            .WithToNumberOfGarages(1)
+            .WithFromArea(100)
+            .WithToArea(200)
+            .WithFromBuiltArea(80)
+            .WithToBuiltArea(160)
+            .WithFromSellingPrice(100000)
+            .WithToSellingPrice(200000)
+            .WithFromRentalTotalPrice(1000)
+            .WithToRentalTotalPrice(3000)
+            .WithFromRentalPrice(1000)
+            .WithToRentalPrice(2000)
+            .WithFromPriceByM2(100)
+            .WithToPriceByM2(200)
+            .WithFromCreatedAt("2023-01-01")
+            .WithToCreatedAt("2023-02-01")
+            .WithFromUpdatedAt("2023-02-01")
+            .WithToUpdatedAt("2023-03-01");
+
+        SearchPropertiesQuery query = queryBuilder.Build();
+
+        _useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(query, PageRequestHelper.Of("0", "1", "UpdatedDate:Desc")))
+            .ReturnsAsync(
+                new Page<Property>(new List<Property>(), new Pageable(0, 1, 0, 0, 0))
+            );
+
+        SearchPropertiesRequest request = new SearchPropertiesRequest
+        {
+            Type = "Apartment",
+            Transaction = "Rent",
+            RefId = "REF 101",
+            City = "White Duck",
+            Districts = new List<string> {"Downtown", "Alta Vista"},
+            NumberOfBedroomsMin = 0,
+            NumberOfBedroomsMax = 3,
+            NumberOfToiletsMin = 1,
+            NumberOfToiletsMax = 2,
+            NumberOfGaragesMin = 1,
+            NumberOfGaragesMax = 1,
+            AreaMin = 100,
+            AreaMax = 200,
+            BuiltAreaMin = 80,
+            BuiltAreaMax = 160,
+            SellingPriceMin = 100000,
+            SellingPriceMax = 200000,
+            RentalTotalPriceMin = 1000,
+            RentalTotalPriceMax = 3000,
+            RentalPriceMin = 1000,
+            RentalPriceMax = 2000,
+            PriceByM2Min = 100,
+            PriceByM2Max = 200,
+            FromCreatedAt = "2023-01-01",
+            ToCreatedAt = "2023-02-01",
+            FromUpdatedAt = "2023-02-01",
+            ToUpdatedAt = "2023-03-01"
+        };
+
+        QPageRequest qPageRequest = new QPageRequest
+        {
+            PageNumber = "0",
+            PageSize = "1",
+            OrderBy = new List<string> {"UpdatedDate,Desc"}
+        };
+        // Act
+        ObjectResult objectResult = await _invoker.InvokeAsync(() => _apiMethod.SearchProperties(request, qPageRequest));
+
+        // Assert
+        Assert.NotNull(objectResult);
+        Assert.Equal((int) HttpStatusCode.NoContent, objectResult.StatusCode);
+    }
+
+    [Trait("Category", "Entrypoint (WebApi) Integration tests")]
+    [Fact(DisplayName = "GET /api/properties/search should return 200 when there is only one page")]
+    public async Task SuccessfulToSearchProperties()
+    {
+        // Arrange
+        SearchPropertiesQueryBuilder queryBuilder = new SearchPropertiesQueryBuilder();
+
+        queryBuilder
+            .WithType("Apartment")
+            .WithTransaction("Rent")
+            .WithRefId("REF 101")
+            .WithCity("White Duck")
+            .WithDistricts(new List<string> {"Downtown", "Alta Vista"})
+            .WithFromNumberOfBedrooms(0)
+            .WithToNumberOfBedrooms(3)
+            .WithFromNumberOfToilets(1)
+            .WithToNumberOfToilets(2)
+            .WithFromNumberOfGarages(1)
+            .WithToNumberOfGarages(1)
+            .WithFromArea(100)
+            .WithToArea(200)
+            .WithFromBuiltArea(80)
+            .WithToBuiltArea(160)
+            .WithFromSellingPrice(100000)
+            .WithToSellingPrice(200000)
+            .WithFromRentalTotalPrice(1000)
+            .WithToRentalTotalPrice(3000)
+            .WithFromRentalPrice(1000)
+            .WithToRentalPrice(2000)
+            .WithFromPriceByM2(100)
+            .WithToPriceByM2(200)
+            .WithFromCreatedAt("2023-01-01")
+            .WithToCreatedAt("2023-02-01")
+            .WithFromUpdatedAt("2023-02-01")
+            .WithToUpdatedAt("2023-03-01");
+
+        SearchPropertiesQuery query = queryBuilder.Build();
+
+        _useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(query, PageRequestHelper.Of("0", "1", "UpdatedDate:Desc")))
+            .ReturnsAsync(
+                new Page<Property>(new List<Property>
+                    {
+                        new Property
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Type = PropertyType.Apartment,
+                            Advertise = new PropertyAdvertise
+                            {
+                                TenantId = 1,
+                                Transaction = Transaction.Rent,
+                                Title = "Apartment for rent",
+                                Description = "Apartment for rent",
+                                Url = "https://www.apartment-for-rent.com",
+                                RefId = "REF 101",
+                                Images = new List<string>
+                                {
+                                    "https://www.apartment-for-rent.com/image1.jpg",
+                                    "https://www.apartment-for-rent.com/image2.jpg"
+                                }
+                            },
+                            Attributes = new PropertyAttributes
+                            {
+                                NumberOfBedrooms = 2,
+                                NumberOfToilets = 1,
+                                NumberOfGarages = 1,
+                                Area = 100,
+                                BuiltArea = 200,
+                            },
+                            Location = new PropertyLocation
+                            {
+                                City = "White Duck",
+                                District = "Downtown",
+                                Address = "Good Life Street, 101",
+                            },
+                            Prices = new PropertyPrices
+                            {
+                                SellingPrice = 100000,
+                                RentalTotalPrice = 1000,
+                                RentalPrice = 1000,
+                                PriceByM2 = 100,
+                                Discount = 100,
+                                CondominiumFee = 90,
+                            },
+                            HashKey = "hash-key-1",
+                            CreatedAt = DateTime.Parse("2023-01-01 23:59:59.999", new CultureInfo("pt-BR")),
+                            UpdatedAt = DateTime.Parse("2023-02-01 23:59:59.999", new CultureInfo("pt-BR")),
+                        }
+                    },
+                    new Pageable(0, 1, 1, 1, 1))
+            );
+
+        SearchPropertiesRequest request = new SearchPropertiesRequest
+        {
+            Type = "Apartment",
+            Transaction = "Rent",
+            RefId = "REF 101",
+            City = "White Duck",
+            Districts = new List<string> {"Downtown", "Alta Vista"},
+            NumberOfBedroomsMin = 0,
+            NumberOfBedroomsMax = 3,
+            NumberOfToiletsMin = 1,
+            NumberOfToiletsMax = 2,
+            NumberOfGaragesMin = 1,
+            NumberOfGaragesMax = 1,
+            AreaMin = 100,
+            AreaMax = 200,
+            BuiltAreaMin = 80,
+            BuiltAreaMax = 160,
+            SellingPriceMin = 100000,
+            SellingPriceMax = 200000,
+            RentalTotalPriceMin = 1000,
+            RentalTotalPriceMax = 3000,
+            RentalPriceMin = 1000,
+            RentalPriceMax = 2000,
+            PriceByM2Min = 100,
+            PriceByM2Max = 200,
+            FromCreatedAt = "2023-01-01",
+            ToCreatedAt = "2023-02-01",
+            FromUpdatedAt = "2023-02-01",
+            ToUpdatedAt = "2023-03-01"
+        };
+
+        QPageRequest qPageRequest = new QPageRequest
+        {
+            PageNumber = "0",
+            PageSize = "1",
+            OrderBy = new List<string> {"UpdatedDate,Desc"}
+        };
+        // Act
+        ObjectResult objectResult = await _invoker.InvokeAsync(() => _apiMethod.SearchProperties(request, qPageRequest));
+
+        // Assert
+        Assert.NotNull(objectResult);
+        Assert.Equal((int) HttpStatusCode.OK, objectResult.StatusCode);
+
+        JsonElement jsonElement = ApiMethodTestHelper.TryGetJsonElement(objectResult);
+
+        _outputHelper.WriteLine(jsonElement.ToString());
+
+        JsonAssertHelper.AssertThat(jsonElement)
+            .AndExpectThat(JsonFrom.Path("$.content"), Is<object>.Single())
+            .AndExpectThat(JsonFrom.Path("$.content[0].id"), Is<string>.NotEmpty())
+            .AndExpectThat(JsonFrom.Path("$.content[0].tenantId"), Is<int>.EqualTo(1))
+            .AndExpectThat(JsonFrom.Path("$.content[0].transaction"), Is<string>.EqualTo("Rent"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].title"), Is<string>.EqualTo("Apartment for rent"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].description"), Is<string>.EqualTo("Apartment for rent"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].url"), Is<string>.EqualTo("https://www.apartment-for-rent.com"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].refId"), Is<string>.EqualTo("REF 101"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].images"), Is<List<string>>.EqualTo(new List<string>
+            {
+                "https://www.apartment-for-rent.com/image1.jpg",
+                "https://www.apartment-for-rent.com/image2.jpg"
+            }))
+            .AndExpectThat(JsonFrom.Path("$.content[0].numberOfBedrooms"), Is<int>.EqualTo(2))
+            .AndExpectThat(JsonFrom.Path("$.content[0].numberOfToilets"), Is<int>.EqualTo(1))
+            .AndExpectThat(JsonFrom.Path("$.content[0].numberOfGarages"), Is<int>.EqualTo(1))
+            .AndExpectThat(JsonFrom.Path("$.content[0].area"), Is<byte>.EqualTo(100))
+            .AndExpectThat(JsonFrom.Path("$.content[0].builtArea"), Is<int>.EqualTo(200))
+            .AndExpectThat(JsonFrom.Path("$.content[0].city"), Is<string>.EqualTo("White Duck"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].district"), Is<string>.EqualTo("Downtown"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].address"), Is<string>.EqualTo("Good Life Street, 101"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].sellingPrice"), Is<int>.EqualTo(100000))
+            .AndExpectThat(JsonFrom.Path("$.content[0].rentalTotalPrice"), Is<int>.EqualTo(1000))
+            .AndExpectThat(JsonFrom.Path("$.content[0].rentalPrice"), Is<int>.EqualTo(1000))
+            .AndExpectThat(JsonFrom.Path("$.content[0].priceByM2"), Is<int>.EqualTo(100))
+            .AndExpectThat(JsonFrom.Path("$.content[0].discount"), Is<int>.EqualTo(100))
+            .AndExpectThat(JsonFrom.Path("$.content[0].condominiumFee"), Is<int>.EqualTo(90))
+            .AndExpectThat(JsonFrom.Path("$.content[0].createdAt"), Is<string>.EqualTo("2023-01-01T23:59:59.999"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].updatedAt"), Is<string>.EqualTo("2023-02-01T23:59:59.999"))
+            .AndExpectThat(JsonFrom.Path("$.content[0].href"), Is<string>.StartWith("http://localhost:7029/api/properties-search/v1/properties/"));
     }
 }

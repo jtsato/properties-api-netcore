997e20fee659629150136871da80920eb937b7b0
jtsato <jorge.takeshi.sato@gmail.com>
2023-09-10 18:53:01 -0300

    feat(wip): work in progress

---
+++ UnitTest.Core/Domains/ServiceResolverInjectionOnValidationTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Core.Domains.Properties.UseCases;
+using Xunit;
+
+namespace UnitTest.Core.Domains;
+
+public class ServiceResolverInjectionOnValidationTest
+{
+    [Trait("Category", "Core Business tests")]
+    [Theory(DisplayName = "Fail to create an instance if service resolver is not supplied")]
+    [InlineData(typeof(GetPropertyByIdUseCase))]
+    [InlineData(typeof(SearchPropertiesUseCase))]
+    public void FailToCreateAnInstanceIfServiceResolverIsNotSupplied(Type type)
+    {
+        // Arrange
+        // Act
+        // Assert
+        TargetInvocationException targetInvocationException = Assert.Throws<TargetInvocationException>(() =>
+            CreateInstance(type)
+        );
+
+        Exception innerException = targetInvocationException.InnerException;
+
+        Assert.NotNull(innerException);
+
+        Assert.Equal(typeof(ArgumentNullException), innerException.GetType());
+        Assert.Equal("Value cannot be null. (Parameter 'serviceResolver')", innerException.Message);
+    }
+
+    [ExcludeFromCodeCoverage]
+    private static void CreateInstance(Type type)
+    {
+        ConstructorInfo[] constructors = type.GetConstructors();
+        ConstructorInfo constructor = constructors[0];
+        object[] parameters = new object[1];
+        constructor.Invoke(parameters);
+    }
+}
--- run.cmd
+++ run.cmd
@@ -1,11 +1,45 @@
 @ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 IF /I "%~1"=="/?" GOTO help
 IF /I "%~1"=="--help" GOTO help
 
 IF /I "%~1"=="clean" GOTO clean
-IF /I "%~1"=="test" GOTO test
 IF /I "%~1"=="app" GOTO app
+
+:: Set the MongoDB host and port for the integration tests 
+SET host=127.0.0.1
+SET port=27018
+SET timeout=30
+
+ECHO.
+ECHO Starting the integration test database...
+CALL docker-compose -f IntegrationTest.Infra.MongoDB/docker-compose.yml up -d
+
+ECHO.
+:wait_loop
+
+ECHO Waiting for MongoDB to start...
+
+TIMEOUT /t 2 /nobreak > nul
+
+CURL --output NUL --silent --fail !host!:!port!
+IF !ERRORLEVEL! equ 0 (
+    ECHO.
+    ECHO MongoDB is running on port !port!.
+    TIMEOUT /t 3 /nobreak > nul
+) ELSE (
+    SET /a timeout-=1
+    IF !timeout! gtr 0 (
+        goto wait_loop
+    ) ELSE (
+        ECHO.
+        ECHO Timed out waiting for MongoDB to start.
+        EXIT /b 1
+    )
+)
+
+IF /I "%~1"=="test" GOTO test
 IF /I "%~1"=="coverage" GOTO coverage
 IF /I "%~1"=="mutation" GOTO mutation
 
@@ -35,9 +69,9 @@ GOTO end
 ECHO.
 ECHO Removing binary files...
 CALL dotnet clean
-for /f %%i in ('dir bin /s /b') do rd /s /q %%i
-for /f %%i in ('dir obj /s /b') do rd /s /q %%i
-for /f %%i in ('dir StrykerOutput /s /b') do rd /s /q %%i
+FOR /f %%i in ('dir bin /s /b') do rd /s /q %%i
+FOR /f %%i in ('dir obj /s /b') do rd /s /q %%i
+FOR /f %%i in ('dir StrykerOutput /s /b') do rd /s /q %%i
 
 IF /I "%~2"=="test" GOTO test
 IF /I "%~2"=="coverage" GOTO coverage
@@ -46,10 +80,6 @@ IF /I "%~2"=="mutation" GOTO mutation
 GOTO end
 
 :test
-ECHO.
-ECHO Starting the integration test database...
-CALL docker-compose -f IntegrationTest.Infra.MongoDB/docker-compose.yml up -d
-
 ECHO.
 ECHO Resolving dependencies...
 CALL dotnet restore --force --no-cache
@@ -85,10 +115,6 @@ GOTO end
 
 :coverage
 ECHO.
-ECHO Starting the integration test database...
-CALL docker-compose -f IntegrationTest.Infra.MongoDB/docker-compose.yml up -d
-
-ECHO.
 ECHO Resolving dependencies...
 CALL dotnet restore --force --no-cache
 
@@ -98,19 +124,32 @@ CALL dotnet build --configuration Debug --no-restore
 
 ECHO.
 ECHO Running test...
-dotnet test --no-build --nologo -v q
+dotnet test --no-build --nologo -v q /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
 
 ECHO.
 ECHO Opening code coverage...
-:: CALL START coverage\lcov-report\index.html
+dotnet tool list -g | FINDSTR /C:"dotnet-reportgenerator-globaltool" > nul
+
+IF %ERRORLEVEL% equ 0 (
+    ECHO dotnet-reportgenerator-globaltool is already installed.
+) ELSE (
+    ECHO Installing dotnet-reportgenerator-globaltool...
+    dotnet tool install -g dotnet-reportgenerator-globaltool
+    IF %ERRORLEVEL% equ 0 (
+        ECHO Installation completed successfully.
+    ) else (
+        ECHO Installation failed.
+        EXIT /b 1
+    )
+)
+
+reportgenerator "-reports:./**/coverage.cobertura.xml" "-targetdir:coverage\lcov-report" "-reporttypes:Html"
+
+start coverage\lcov-report\index.html
 
 GOTO end
 
 :mutation
-ECHO.
-ECHO Starting the integration test database...
-CALL docker-compose -f IntegrationTest.Infra.MongoDB/docker-compose.yml up -d
-
 ECHO.
 ECHO Resolving dependencies...
 CALL dotnet restore --force --no-cache
